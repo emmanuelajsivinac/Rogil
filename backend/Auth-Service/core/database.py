240ef37de77faa7e745965cfead0c40e467ee634
emmanuelajsivinac <emmajsivinac@gmail.com>
2024-10-14 17:59:31 -0600

    first version api auth service get menus put user login info[200~~

--- backend/Auth-Service/core/database.py
+++ backend/Auth-Service/core/database.py
@@ -1,11 +1,27 @@
-import redis
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+load_dotenv()
+print(f'mysql+pymysql://{os.getenv('USER_DB')}:{os.getenv('PASSWORD_DB')}@{os.getenv('HOST_DB')}:{os.getenv('PORT_DB')}/{os.getenv('DATABASE')}')
 
-client = redis.StrictRedis(host='192.168.0.8', port=6379, db=0)
+DB_URL = f'mysql+pymysql://{os.getenv('USER_DB')}:{os.getenv('PASSWORD_DB')}@{os.getenv('HOST_DB')}:{os.getenv('PORT_DB')}/{os.getenv('DATABASE')}'
 
+engine = create_engine(DB_URL, connect_args={'charset':'utf8mb4'}, pool_pre_ping=True)
 
-try:
-    client.ping()
-    print("Conectado a Redis")
-except redis.ConnectionError:
-    print("No se pudo conectar a Redis")
+sesionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+baseDB = declarative_base()
+
+class Database:
+    def __init__(self):
+        self.connection = sesionLocal()
+
+    def get_connection(self):
+        return self.connection
+    
+    def close(self):
+        self.connection.close()
+
+handlerDB = Database()
