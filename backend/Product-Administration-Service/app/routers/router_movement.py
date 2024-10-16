bcb0d1bbc5b3efb4f670390a343874bdd141104f
Emmanuel Ajsivinac <emmajsivinac@gmail.com>
2024-10-15 21:44:56 -0600

    Update router_movement.py deleted comment

--- backend/Product-Administration-Service/app/routers/router_movement.py
+++ backend/Product-Administration-Service/app/routers/router_movement.py
@@ -92,38 +92,4 @@ def delete_inventory_movement(id_movement: int, db: Session = Depends(get_db)):
 
 
 
-# @router.post("/movimientos/", response_model=schemas.Movimiento)
-# def crear_movimiento(movimiento: schemas.MovimientoCreate, db: Session = Depends(get_db)):
-#     # Verificar si el inventario existe
-#     inventario = db.query(models.Inventario).filter(models.Inventario.id_inventario == movimiento.id_inventario).first()
 
-#     if not inventario:
-#         raise HTTPException(status_code=404, detail="Inventario no encontrado")
-
-#     # Verificar si es una salida y si hay suficiente stock
-#     if movimiento.tipo_movimiento == schemas.TipoMovimiento.SALIDA:
-#         if inventario.cantidad_stock < movimiento.cantidad:
-#             raise HTTPException(status_code=400, detail="No hay suficiente stock para realizar la salida")
-
-#     # Actualizar el stock según el tipo de movimiento
-#     if movimiento.tipo_movimiento == schemas.TipoMovimiento.ENTRADA:
-#         inventario.cantidad_stock += movimiento.cantidad
-#     elif movimiento.tipo_movimiento == schemas.TipoMovimiento.SALIDA:
-#         inventario.cantidad_stock -= movimiento.cantidad
-
-#     # Crear el registro del movimiento
-#     nuevo_movimiento = models.Movimiento(
-#         id_inventario=movimiento.id_inventario,
-#         tipo_movimiento=movimiento.tipo_movimiento,  # Aquí el mapeo es correcto
-#         cantidad=movimiento.cantidad,
-#         empleado=movimiento.empleado,
-#         total=inventario.precio_venta * movimiento.cantidad 
-#     )
-
-#     # Agregar el nuevo movimiento a la sesión
-#     db.add(nuevo_movimiento)
-#     db.commit()
-#     db.refresh(nuevo_movimiento)  # Refresca para obtener los datos del movimiento
-
-#     # Retorna el nuevo movimiento como respuesta
-#     return nuevo_movimiento
